a31e59b7cda84bd4319e73f8f13bc0ba9a0b857c
Chris Costes <costes.c@gmail.com>
2018-04-29 13:10:53 -0400

    Added Header

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -4,11 +4,16 @@ import { StatusBar } from 'react-native';
 import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
 import { InputWithButton } from '../components/TextInput';
+import { ClearButton } from '../components/Buttons';
+import { LastConverted } from '../components/Text';
+import { Header } from '../components/Header';
 
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'GBP';
 const TEMP_BASE_PRICE = '100';
 const TEMP_QUOTE_PRICE = '79.74';
+const TEMP_CONVERSION_RATE = 0.7974
+const TEMP_CONVERSION_DATE = new Date();
 
 class Home extends Component {
   handlePressBaseCurrency = () => {
@@ -22,11 +27,23 @@ class Home extends Component {
   handleTextChange = (text) => {
     console.log('change text', text);
   };
+  
+  handleSwapCurrency = (text) => {
+    console.log('swap currency');
+  };
+  
+  handleOptionsPress = (text) => {
+    console.log('options press');
+  };
+
 
   render() {
     return (
       <Container>
         <StatusBar translucent={false} barStyle="light-content" />
+        <Header
+          onPress={this.handleOptionsPress}
+        />
         <Logo />
         <InputWithButton
           buttonText={TEMP_BASE_CURRENCY}
@@ -40,6 +57,16 @@ class Home extends Component {
           onPress={this.handlePressQuoteCurrency}
           editable={false}
           value={TEMP_QUOTE_PRICE}
+        />
+        <LastConverted
+          base={TEMP_BASE_CURRENCY}
+          quote={TEMP_QUOTE_CURRENCY}
+          date={TEMP_CONVERSION_DATE}
+          conversionRate={TEMP_CONVERSION_RATE}
+        />
+        <ClearButton
+          text="Reverse Currencies"
+          onPress={this.handleSwapCurrency}
         />
       </Container>
     );
